ee18341326363925f7e3875146dbf27d957a00e2
Nicholas Rodgers <nick.rodgers42@gmail.com>
2016-11-04 15:14:10 -0600

    update riddle.h

--- riddle.h
+++ riddle.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef RIDDLE
+#define RIDDLE
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Riddle
+{
+private:
+  string question, answer;
+
+public:
+  Riddle();
+  Riddle(string q, string a);
+  ~Riddle();
+  string getQuestion(void);
+  string getAnswer(void);
+  bool giveAnswer(string a);
+};
+
+
+#endif
